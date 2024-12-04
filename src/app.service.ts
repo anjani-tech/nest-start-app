12cb485d050632472fdc529411776627c7dff888
anjani-tech <77617679+anjani-tech@users.noreply.github.com>
2024-12-04 10:25:13 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from CI CD!';
+    return 'Hello World from CI CD!!!!';
   }
 }
